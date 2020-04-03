0df011d772d6789e55736f9f521b9ff8c6e21f26
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-04-03 19:25:04 +0200

    Add a Square class

---
+++ lib/square.rb
@@ -0,0 +1,25 @@
+class Square
+
+  attr_accessor :color, :piece
+
+  def initialize(color:, piece: nil)
+    # each square has a color ("w" or "b") and an optional piece on it
+    @color = color
+    @piece = piece
+  end
+
+  def occupied?
+    !@piece.nil?
+  end
+
+  def to_s
+    # either return piece.to_s or a square with color
+    if occupied?
+      @piece.to_s
+    elsif @color == "w"
+      "□"
+    else
+      "■"
+    end
+  end
+end
